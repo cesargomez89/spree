06ab2ae5e0c53daddad3e1ce6ad1dca4155f1d2f
Cesar Gomez <cesargomez89@gmail.com>
2013-06-26 18:20:05 -0500

    first commit - empty rails app with spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree::Application.config.secret_token = '91d28d431d54a6a2e9cefbc0badb755231d9c0b2db6ffed837e45b65e3ce18e8e82cb358d72887e5f1ad9d09e355aa118680fcf63ee03c2d83d51e187a6d38e2'
